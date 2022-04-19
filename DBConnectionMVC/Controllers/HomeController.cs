910d662480fcc38890a698e765c8736eeb934ae8
Kenan <gurbanov.kanan02@gmail.com>
2022-04-19 19:52:36 +0400

    Add project files.

---
+++ DBConnectionMVC/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using DBConnectionMVC.Data;
+using DBConnectionMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBConnectionMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TestDB _context;
+        public HomeController(ILogger<HomeController> logger,TestDB context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            User user =await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(user);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddUser()
+        {
+        
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public IActionResult GetAll( )
+        {
+            return View("Index",_context.Users.ToList());
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DBConnectionMVC/Data/TestDB.cs
@@ -0,0 +1,17 @@
+﻿using DBConnectionMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBConnectionMVC.Data
+{
+    public class TestDB:DbContext
+    {
+        public TestDB(DbContextOptions<TestDB> options):base(options)
+        {
+           
+             Database.EnsureCreated();
+          
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
